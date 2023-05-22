aba41e1fa1035477acfad20eafc62cf1f4b15ed8
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-22 09:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const res = document.getElementById("output");
+
+const promises = [
+	new Promise((resolve) =>{
+	const time = Math.floor(Math.random() *3 +1) * 1000;
+	setTimeOut(() => resolve ({ name:"promise 1", time: time/1000}), 1000);
+	}),
+	new Promise((resolve) =>{
+	const time = Math.floor(Math.random() *3 +1) * 1000;
+	setTimeOut(() => resolve ({ name:"promise 2", time: time/1000}), 1000);
+	}),
+	new Promise((resolve) =>{
+	const time = Math.floor(Math.random() *3 +1) * 1000;
+	setTimeOut(() => resolve ({ name:"promise 3", time: time/1000}), 1000);
+	})
+];
+
+async function callfn(){
+	await Promise.all(promises).then((result)=>{
+		res.innerHTML=``;
+		reult.forEach((item)=>{
+			res.innerHTML +=`
+   <tr>
+   <td>$(item.name)</td>
+   <td>$(item.time)</td>
+   </tr>
+   `;
+	});
+	}).catch((err)=>{
+		console.log(err);
+	});
+}
+
+callfn();
